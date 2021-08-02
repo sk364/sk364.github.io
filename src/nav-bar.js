806223e08e7f3018108f9fe462755a990e63895f
Sachin <skad5455@gmail.com>
2021-08-02 20:53:05 +0530

    Update content

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -6,34 +6,36 @@ const NavBar = () => {
   const history = useHistory();
 
   const items = [{
-    id: "projects",
-    name: "Projects",
-    route: "/projects"
+    id: "resume",
+    name: "Resume",
+    route: "./resume.pdf",
+    icon: "./resume.png"
   }, {
-    id: "logs",
-    name: "Logs",
-    route: "/logs"
+    id: "github",
+    name: "Github",
+    route: "https://github.com/sk364",
+    icon: "./github.png"
   }, {
-    id: "contact",
-    name: "Contact",
-    route: "/contact"
+    id: "li",
+    name: "Linked In",
+    route: "https://www.linkedin.com/in/supah-hakah",
+    icon: "./linkedIn.png"
   }];
-
-  const handleChangePage = (route) => {
-    history.push(route);
-  };
 
   return (
     <div className="nav-bar">
-      <div className="brand" onClick={() => handleChangePage("/")}>
-        @sk364
+      <div className="brand">
+        Sachin Kukreja
       </div>
 
       <div className="nav-items">
         {items.map(item => {
           return (
-            <div className="nav-item" key={item.id} onClick={() => handleChangePage(item.route)}>
-              { item.name }
+            <div className="nav-item" key={item.id}>
+              <img src={item.icon} className={`icon ${item.id}`} alt="icon" />
+              {item.route && (
+                <a href={item.route} target="_blank">{item.name}</a>
+              )}
             </div>
           );
         })}
