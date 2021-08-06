fef4bc681dbfdd4942e485c71be57f6e28d73c8c
Sachin <skad5455@gmail.com>
2021-08-06 22:19:26 +0530

    Add post

--- src/constants.js
+++ src/constants.js
@@ -1,33 +1,10 @@
-export const POSTS = [];
-/*[
+export const POSTS = [
   {
     id: 1,
-    title: "Tick tock goes the clock",
-    bait: "How much time have we lost?",
-    tags: ["opinion", "mind"],
+    title: "React on Github",
+    bait: "Combine the powers of gh-pages and create-react-app!",
+    tags: ["tutorial", "react", "github"],
     numMinRead: 3,
-    publishedAt: "2021-04-01T00:00:00",
-    content: `
-      <div class="post-title">{title}</div>
-      <div class="post-bait">{bait}</div>
-      <span>
-        <div class="post-reading-time right-circle">{numMinRead} min read</div>
-        <div class="post-published-since">{publishedSince}</div>
-      </span>
-      <div class="post-tags">{tags}</div>
-      <div class="post-image">
-        <img src="./github.png" />
-      </div>
-      <div class="post-text">Config gist above! ^</div>
-    `
-  },
-  {
-    id: 2,
-    title: "When Zarathustra Spoke",
-    bait: "Friedrich Nietzsche imparting the age-old wisdom",
-    tags: ["book"],
-    numMinRead: 6,
-    publishedAt: "2021-04-30T00:00:00",
-    content: "<div>bbb</div>"
+    publishedAt: "2021-08-07T00:00:00",
   }
-];*/
+];
