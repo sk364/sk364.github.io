7491fafd868fb3c282d9f53fd1003c6bb0b0ba98
Sachin <skad5455@gmail.com>
2022-03-23 20:46:35 +0530

    Comment code

--- src/constants.js
+++ src/constants.js
@@ -7,6 +7,14 @@ export const POSTS = [
     numMinRead: 3,
     publishedAt: "2021-08-07T00:00:00",
   },
+  // {
+  //   id: 3,
+  //   title: "Descartes' Meditations on First Philosophy (Part 1)",
+  //   bait: "Journey into the Descartes' mind",
+  //   tags: ["philosophy", "descartes", "summary"],
+  //   numMinRead: 10,
+  //   publishedAt: "2021-11-07T00:00:00",
+  // },
   {
     id: 4,
     title: "Finding research papers",
